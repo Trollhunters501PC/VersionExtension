3b32ba03229e2dc2a00ed5b5fec41a6f4a2908a5
Trollhunters501PC <cpcreador@gmail.com>
2024-12-12 13:20:18 -0600

    NUEVO

--- src/main/java/me/petterim1/versionextension/Main.java
+++ src/main/java/me/petterim1/versionextension/Main.java
@@ -15,13 +15,13 @@ public class Main extends PluginBase {
         List<Integer> versions = getConfig().getIntegerList("extraVersions");
         Class<?> c = ProtocolInfo.class;
         try {
-            Field f1 = c.getDeclaredField("CURRENT_PROTOCOL");
+            Field f1 = c.getDeclaredField("ENABLED_PROTOCOLS");
             f1.setAccessible(true);
             int currentProtocol = f1.getInt(null);
             getLogger().debug("Current protocol: " + currentProtocol);
             versions.add(currentProtocol);
             getLogger().debug("Versions: " + versions.toString());
-            Field f2 = c.getDeclaredField("SUPPORTED_PROTOCOLS");
+            Field f2 = c.getDeclaredField("ENABLED_PROTOCOLS");
             f2.setAccessible(true);
             Field m = Field.class.getDeclaredField("modifiers");
             m.setAccessible(true);
